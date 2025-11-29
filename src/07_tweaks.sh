2920d445d1b091b840f14c5934d672d77d99af74
nhosh <sulejmani.niels@gmail.com>
2025-11-29 21:20:03 +0100

    Always suppress verbose docker logs

--- src/07_tweaks.sh
+++ src/07_tweaks.sh
@@ -1,14 +1,14 @@
 # tweaks.sh
 
+# Docker: do not use progress bars in CI, and flood the logs
+export BUILDKIT_PROGRESS=plain
+
 # No TTY connected?
 if [ ! -t 1 ]; then
-    # Docker: do not use progress bars in CI, and flood the logs
-    export BUILDKIT_PROGRESS=plain
-
     # Do not use colored output if there is not TTY, only causes trouble
     export NO_COLOR=1
 
-    # Simplify terminal output, to avoid toruble
+    # Simplify terminal output, to avoid trouble
     export TERM=dumb
 
     # TODO What else can we by default make better in non-TTY?
